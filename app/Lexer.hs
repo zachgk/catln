422a7f2885d0878de6262c656616799813294a8e
Zach Kimberg <zachary@kimberg.com>
2019-09-15 18:43:08 -0700

    Add tutorial Chapter 7

--- app/Lexer.hs
+++ app/Lexer.hs
@@ -1,41 +1,47 @@
+--------------------------------------------------------------------
+-- |
+-- Module    :  Lexer
+-- Copyright :  (c) Stephen Diehl 2013
+-- License   :  MIT
+-- Maintainer:  stephen.m.diehl@gmail.com
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------
+
 module Lexer where
 
 import Text.Parsec.String (Parser)
 import Text.Parsec.Language (emptyDef)
+import Text.Parsec.Prim (many)
 
 import qualified Text.Parsec.Token as Tok
 
 lexer :: Tok.TokenParser ()
 lexer = Tok.makeTokenParser style
   where
-    ops = ["+","*","-",";"]
-    names = ["def","extern"]
+    ops = ["+","*","-","/",";","=",",","<",">","|",":"]
+    names = ["def","extern","if","then","else","in","for"
+            ,"binary", "unary", "var"
+            ]
     style = emptyDef {
                Tok.commentLine = "#"
              , Tok.reservedOpNames = ops
              , Tok.reservedNames = names
              }
 
-integer :: Parser Integer
-integer = Tok.integer lexer
-
-float :: Parser Double
-float = Tok.float lexer
-
-parens :: Parser a -> Parser a
-parens = Tok.parens lexer
-
-commaSep :: Parser a -> Parser [a]
-commaSep = Tok.commaSep lexer
-
-semiSep :: Parser a -> Parser [a]
-semiSep = Tok.semiSep lexer
-
-identifier :: Parser String
+integer    = Tok.integer lexer
+float      = Tok.float lexer
+parens     = Tok.parens lexer
+commaSep   = Tok.commaSep lexer
+semiSep    = Tok.semiSep lexer
 identifier = Tok.identifier lexer
-
-reserved :: String -> Parser ()
-reserved = Tok.reserved lexer
-
-reservedOp :: String -> Parser ()
+whitespace = Tok.whiteSpace lexer
+reserved   = Tok.reserved lexer
 reservedOp = Tok.reservedOp lexer
+
+operator :: Parser String
+operator = do
+  c <- Tok.opStart emptyDef
+  cs <- many $ Tok.opLetter emptyDef
+  return (c:cs)
--- app/Parser.hs
+++ app/Parser.hs
@@ -1,7 +1,19 @@
+--------------------------------------------------------------------
+-- |
+-- Module    :  Parser
+-- Copyright :  (c) Stephen Diehl 2013
+-- License   :  MIT
+-- Maintainer:  stephen.m.diehl@gmail.com
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------
+
 module Parser where
 
 import Text.Parsec
 import Text.Parsec.String (Parser)
+import Control.Applicative ((<$>))
 
 import qualified Text.Parsec.Expr as Ex
 import qualified Text.Parsec.Token as Tok
@@ -9,36 +21,42 @@ import qualified Text.Parsec.Token as Tok
 import Lexer
 import Syntax
 
-binary s f assoc = Ex.Infix (reservedOp s >> return (BinOp f)) assoc
-
-table = [[binary "*" Times Ex.AssocLeft,
-          binary "/" Divide Ex.AssocLeft]
-        ,[binary "+" Plus Ex.AssocLeft,
-          binary "-" Minus Ex.AssocLeft]]
-
 int :: Parser Expr
-int = do
-  n <- integer
-  return $ Float (fromInteger n)
+int = Int <$> integer
 
 floating :: Parser Expr
-floating = do
-  n <- float
-  return $ Float n
+floating = Float <$> float
+
+binop = Ex.Infix (BinaryOp <$> op) Ex.AssocLeft
+unop = Ex.Prefix (UnaryOp <$> op)
+
+binary s assoc = Ex.Infix (reservedOp s >> return (BinaryOp s)) assoc
+
+op :: Parser String
+op = do
+  whitespace
+  o <- operator
+  whitespace
+  return o
+
+binops = [[binary "=" Ex.AssocLeft]
+        ,[binary "*" Ex.AssocLeft,
+          binary "/" Ex.AssocLeft]
+        ,[binary "+" Ex.AssocLeft,
+          binary "-" Ex.AssocLeft]
+        ,[binary "<" Ex.AssocLeft]]
 
 expr :: Parser Expr
-expr = Ex.buildExpressionParser table factor
+expr =  Ex.buildExpressionParser (binops ++ [[unop], [binop]]) factor
 
 variable :: Parser Expr
-variable = do
-  var <- identifier
-  return $ Var var
+variable = Var <$> identifier
 
 function :: Parser Expr
 function = do
   reserved "def"
   name <- identifier
-  args <- parens $ many variable
+  args <- parens $ many identifier
   body <- expr
   return $ Function name args body
 
@@ -46,7 +64,7 @@ extern :: Parser Expr
 extern = do
   reserved "extern"
   name <- identifier
-  args <- parens $ many variable
+  args <- parens $ many identifier
   return $ Extern name args
 
 call :: Parser Expr
@@ -55,18 +73,76 @@ call = do
   args <- parens $ commaSep expr
   return $ Call name args
 
+ifthen :: Parser Expr
+ifthen = do
+  reserved "if"
+  cond <- expr
+  reserved "then"
+  tr <- expr
+  reserved "else"
+  fl <- expr
+  return $ If cond tr fl
+
+for :: Parser Expr
+for = do
+  reserved "for"
+  var <- identifier
+  reservedOp "="
+  start <- expr
+  reservedOp ","
+  cond <- expr
+  reservedOp ","
+  step <- expr
+  reserved "in"
+  body <- expr
+  return $ For var start cond step body
+
+letins :: Parser Expr
+letins = do
+  reserved "var"
+  defs <- commaSep $ do
+    var <- identifier
+    reservedOp "="
+    val <- expr
+    return (var, val)
+  reserved "in"
+  body <- expr
+  return $ foldr (uncurry Let) body defs
+
+unarydef :: Parser Expr
+unarydef = do
+  reserved "def"
+  reserved "unary"
+  o <- op
+  args <- parens $ many identifier
+  body <- expr
+  return $ UnaryDef o args body
+
+binarydef :: Parser Expr
+binarydef = do
+  reserved "def"
+  reserved "binary"
+  o <- op
+  prec <- int
+  args <- parens $ many identifier
+  body <- expr
+  return $ BinaryDef o args body
+
 factor :: Parser Expr
 factor = try floating
       <|> try int
-      <|> try extern
-      <|> try function
       <|> try call
-      <|> variable
-      <|> parens expr
+      <|> try variable
+      <|> ifthen
+      <|> try letins
+      <|> for
+      <|> (parens expr)
 
 defn :: Parser Expr
 defn = try extern
     <|> try function
+    <|> try unarydef
+    <|> try binarydef
     <|> expr
 
 contents :: Parser a -> Parser a
