8246b6a8e5dce74b379cc04caee4ddf746364fff
Zach Kimberg <zachary@kimberg.com>
2020-09-06 12:02:41 -0700

    Build main repl with IO
    The previous repl was commented out due to no support for IO making it
    impossible. I recreated a new one with the IO support added in a previous commit.

--- app/Main.hs
+++ app/Main.hs
@@ -2,29 +2,38 @@
 
 module Main where
 
--- import qualified Data.HashMap.Strict as H
+import qualified Data.HashMap.Strict as H
 
-import           Desugarf                 (desFiles)
+import           Desugarf                 (desFiles, desPrgm)
 -- import           Emit                     (codegen, initModule)
 import           Eval
 import           Syntax
+import           Syntax.Types
+import           Syntax.Prgm
+import           Parser
+import           Parser.Syntax
 import           TypeCheck.Common  (TypeCheckResult(TypeCheckResult, TypeCheckResE))
 import           TypeCheck                (typecheckPrgm)
 
 import           Control.Monad
--- import           Control.Monad.Trans
--- import           Data.List                (isPrefixOf)
+import           Control.Monad.Trans
+import           Data.List                (isPrefixOf)
 
--- import           System.Console.Haskeline
+import           System.Console.Haskeline
 import           System.Environment
 
--- parsingRepl :: Env -> String -> IO Env
--- parsingRepl env source = case parseRepl source of
---     ReplErr err   -> print err >> return env
---     ReplExpr expr -> print expr >> return env
---     ReplStatement statement -> print statement >> return env
+type ReplEnv = [RawStatement PreTyped]
 
--- genRepl :: Env -> String -> IO Env
+replBaseEnv :: ReplEnv
+replBaseEnv = []
+
+parsingRepl :: ReplEnv -> String -> IO ReplEnv
+parsingRepl env source = case parseRepl source of
+    ReplErr err   -> print err >> return env
+    ReplExpr expr -> print expr >> return env
+    ReplStatement statement -> print statement >> return env
+
+-- genRepl :: ReplEnv -> String -> IO ReplEnv
 -- genRepl env source = do
 --   let res = parseRepl source
 --   case res of
@@ -34,45 +43,56 @@ import           System.Environment
 --         TypeCheckResE err    -> print ("type check err: " ++ show err) >> return env
 --         TypeCheckResult _ tprgm -> codegen initModule tprgm >> return env
 
--- processRepl :: Env -> String -> IO Env
--- processRepl env source = do
---   let res = parseRepl source
---   case res of
---     ReplErr err -> print err >> return env
---     ReplExpr expr -> print (evalExpr env expr) >> return env
---     ReplDecl decl -> case addDecl env (head $ desDecl decl) of
---       Left err'  -> print err' >> return env
---       Right env' -> return env'
+processDes :: CRes DesPrgm -> IO ()
+processDes des = case des of
+  CErr err   -> print err
+  CRes _ prgm ->
+    case typecheckPrgm prgm of
+      TypeCheckResE err -> print err
+      TypeCheckResult _ tprgm ->
+        case evalMain tprgm of
+          CErr err -> print err
+          CRes _ resIO -> do
+            returnValue <- resIO
+            case returnValue of
+              0 -> return ()
+              _ -> print $ "error code " ++ show returnValue
+
+processRepl :: ReplEnv -> String -> IO ReplEnv
+processRepl env source = do
+  let res = parseRepl source
+  case res of
+    ReplErr err -> print err >> return env
+    ReplExpr expr -> do
+      -- main(IO io) = io.println(msg=expr.toString)
+      let exprAsMain = RawDeclStatement $ RawDecl (DeclLHS emptyMeta (Pattern (Object emptyMeta FunctionObj "main" H.empty (H.singleton "io" (PreTyped $ singletonType (PTypeName "IO", H.empty, H.empty), Nothing))) NoGuard)) [] (Just $ RawMethods (RawValue emptyMeta "io") [RawTupleApply emptyMeta (emptyMeta, RawValue emptyMeta "println") [RawTupleArgNamed "msg" (RawMethods expr [RawValue emptyMeta "toString"])]])
+      let rawPrgm = (["std/std.ct"], exprAsMain:env)
+      des <- desPrgm rawPrgm
+      processDes des
+      return env
+    ReplStatement s -> return (s:env)
 
 process :: [String] -> IO ()
 process source = do
   des <- desFiles source
-  case des of
-    CErr err   -> print err
-    CRes _ prgm ->
-      case typecheckPrgm prgm of
-        TypeCheckResE err -> print err
-        TypeCheckResult _ tprgm ->
-          case evalMain tprgm of
-            CErr err -> print err
-            CRes _ res -> print res
+  processDes des
 
 processFile :: String -> IO ()
 processFile fname = process [fname, "std/std.ct"]
 
 repl :: IO ()
-repl = error "The repl is currently not supported"
--- repl = runInputT defaultSettings (loop baseEnv)
---   where loop env = do
---           minput <- getInputLine "eval> "
---           case minput of
---             Nothing -> outputStrLn "Goodbye."
---             Just input -> do
---               env' <- case input of
---                 _ | ":p " `isPrefixOf` input -> lift $ parsingRepl env (drop 3 input)
---                 _ | ":g " `isPrefixOf` input -> lift $ genRepl env (drop 3 input)
---                 _ -> lift $ processRepl env input
---               loop env'
+repl = runInputT defaultSettings (loop replBaseEnv)
+  where loop env = do
+          minput <- getInputLine "eval> "
+          case minput of
+            Nothing -> outputStrLn "Goodbye."
+            Just input | ":q" `isPrefixOf` input -> outputStrLn "Goodbye."
+            Just input -> do
+              env' <- case input of
+                _ | ":p " `isPrefixOf` input -> lift $ parsingRepl env (drop 3 input)
+                -- _ | ":g " `isPrefixOf` input -> lift $ genRepl env (drop 3 input)
+                _ -> lift $ processRepl env input
+              loop env'
 
 main :: IO ()
 main = do
