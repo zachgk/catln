59dce0410bb65a8dd07ebccbb30e3c44e7a820aa
Zach Kimberg <zachary@kimberg.com>
2020-11-22 11:11:26 -0800

    Webdocs runs on request
    Before, it would run all of the processing up front and then serve the results
    on request. Now, it runs the processing on request. The first benefit of this is
    that if it fails during a stage, it can still return earlier stages. In
    addition, it also makes it faster to update the code and view the results
    because it doesn't require restarting the server.

--- src/WebDocs.hs
+++ src/WebDocs.hs
@@ -26,6 +26,8 @@ import TypeCheck (typecheckPrgmWithTrace)
 import           Eval (evalMain)
 import Emit (initModule, codegen)
 import Parser (readFiles)
+import Parser.Syntax (DesPrgm, PPrgm)
+import TypeCheck.Common (TraceConstrain, VPrgm, TPrgm)
 
 data ResSuccess a n = Success a [n]
   deriving (Generic, ToJSON)
@@ -37,46 +39,68 @@ maybeJson :: ToJSON a => CRes a -> ActionM ()
 maybeJson (CRes notes r) = json $ Success r (map show notes)
 maybeJson (CErr notes) = json $ ResFail (map show notes)
 
+getRawPrgm :: Bool -> String -> IO (CRes [(String, PPrgm)])
+getRawPrgm includeStd fileName = readFiles (fileName : ["std/std.ct" | includeStd])
+
+getPrgm :: Bool -> String -> IO (CRes DesPrgm)
+getPrgm includeStd fileName = do
+  base <- getRawPrgm includeStd fileName
+  return (base >>= desFiles)
+
+getTPrgmWithTrace :: Bool -> String -> IO (CRes (TPrgm, VPrgm, TraceConstrain))
+getTPrgmWithTrace includeStd fileName = do
+  base <- getPrgm includeStd fileName
+  return (base >>= typecheckPrgmWithTrace)
+
+getTPrgm :: Bool -> String -> IO (CRes TPrgm)
+getTPrgm includeStd fileName = do
+  base <- getTPrgmWithTrace includeStd fileName
+  return ((\(a, _, _) -> a) <$> base)
+
+getEvaluated :: Bool -> String -> IO Integer
+getEvaluated includeStd fileName = do
+  base <- getTPrgm includeStd fileName
+  let pre = base >>= evalMain
+  case pre of
+    CRes _ r -> r
+    CErr _ -> return 999
+
+getLlvm :: Bool -> String -> IO String
+getLlvm includeStd fileName = do
+  base <- getTPrgm includeStd fileName
+  let pre = base >>= return . codegen initModule
+  case pre of
+    CRes _ r -> BSU.toString <$> r
+    CErr _ -> return ""
+
+
 docServe :: Bool -> String -> IO ()
 docServe includeStd fileName = do
 
-  maybeRawPrgm <- readFiles (fileName : ["std/std.ct" | includeStd])
-
-  let maybePrgm = maybeRawPrgm >>= desFiles
-
-  let maybeTprgmWithTrace = maybePrgm >>= typecheckPrgmWithTrace
-
-  let maybeTprgm = (\(a, _, _) -> a) <$> maybeTprgmWithTrace
-
-  let maybeEvalMainPre = maybeTprgm >>= evalMain
-  evaluated <- case maybeEvalMainPre of
-        CRes _ r -> r
-        CErr _ -> return 999
-
-  let maybeLlvmPre = maybeTprgm >>= return . codegen initModule
-  llvm <- case maybeLlvmPre of
-        CRes _ r -> r
-        CErr _ -> return ""
-
   scotty 31204 $ do
     get "/files" $ do
-      json ["File: ", T.pack fileName]
+      json $ Success ["File: ", T.pack fileName] ([] :: String)
 
     get "/raw" $ do
+      maybeRawPrgm <- liftAndCatchIO $ getRawPrgm includeStd fileName
       maybeJson maybeRawPrgm
 
     get "/desugar" $ do
+      maybePrgm <- liftAndCatchIO $ getPrgm includeStd fileName
       maybeJson maybePrgm
 
     get "/constrain" $ do
+      maybeTprgmWithTrace <- liftAndCatchIO $ getTPrgmWithTrace includeStd fileName
       maybeJson maybeTprgmWithTrace
 
     get "/typecheck" $ do
+      maybeTprgm <- liftAndCatchIO $ getTPrgm includeStd fileName
       maybeJson maybeTprgm
 
     get "/eval" $ do
+      evaluated <- liftAndCatchIO $ getEvaluated includeStd fileName
       json $ Success evaluated ([] :: [String])
 
     get "/llvm" $ do
-      let llvmStr = BSU.toString llvm
-      json $ Success llvmStr ([] :: [String])
+      llvm <- liftAndCatchIO $ getLlvm includeStd fileName
+      json $ Success llvm ([] :: [String])
