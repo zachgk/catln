7c4e6f1c8c8a848caa9eef488c925a297d3b4d50
Zach Kimberg <zachary@kimberg.com>
2021-05-08 00:00:59 -0700

    Add build and doc commands to CLI
    Currently, the CLI has three commands:
    - Run to execute the mainx
    - Build to build the main CatlnResult and save to the build directory
    - Doc to serve the doc API

--- app/Main.hs
+++ app/Main.hs
@@ -5,45 +5,92 @@ import           Desugarf                 (desFiles)
 import           Eval
 import           CRes
 import           Parser
-import           Parser.Syntax
 import           TypeCheck                (typecheckPrgm)
 import Options.Applicative
 
-import           Control.Monad
 import           Data.Semigroup ((<>))
+import Eval.Common (Val(TupleVal, StrVal))
+import qualified Data.HashMap.Strict as H
+import Data.Maybe
+import WebDocs (docServe)
+import System.Directory
+import Text.Printf
 -- import Repl (repl)
 
-processDes :: String -> CRes PPrgmGraphData -> IO ()
-processDes prgmName maybeRawPrgm = case aux of
-  CErr err   -> print err
-  CRes _ resIO -> do
-    returnValue <- resIO
-    case returnValue of
-      (0, _) -> return ()
-      (i, _) -> print $ "error code " ++ show i
+xRun :: String -> IO ()
+xRun prgmName = do
+  maybeRawPrgm <- readFiles True [prgmName]
+  case aux maybeRawPrgm of
+    CErr err   -> print err
+    CRes _ resIO -> do
+      returnValue <- resIO
+      case returnValue of
+        (0, _) -> return ()
+        (i, _) -> print $ "error code " ++ show i
   where
-    aux = do
+    aux maybeRawPrgm = do
       rawPrgm <- maybeRawPrgm
       desPrgm <- desFiles rawPrgm
       tprgm <- typecheckPrgm desPrgm
       evalMainx prgmName tprgm
 
-process :: String -> IO ()
-process source = do
-  raw <- readFiles True [source]
-  processDes source raw
+xBuild :: String -> IO ()
+xBuild prgmName = do
+  maybeRawPrgm <- readFiles True [prgmName]
+  case aux maybeRawPrgm of
+    CErr err   -> print err
+    CRes _ resIO -> do
+      returnValue <- resIO
+      case returnValue of
+        (TupleVal _ args, _) -> do
+          let buildDir = "build"
+          removePathForcibly buildDir
+          createDirectoryIfMissing True buildDir
+          let (StrVal outFileName) = fromJust $ H.lookup "name" args
+          let (StrVal outContents) = fromJust $ H.lookup "contents" args
+          writeFile (buildDir ++ "/" ++ outFileName) outContents
+          printf "Successfully built %s" (show prgmName)
+        _ -> error "Failed to build"
+  where
+    aux maybeRawPrgm = do
+      rawPrgm <- maybeRawPrgm
+      desPrgm <- desFiles rawPrgm
+      tprgm <- typecheckPrgm desPrgm
+      evalMain prgmName tprgm
 
-runFile :: Parser String
-runFile = argument str (metavar "FILE" <> help "The file to run")
+xDoc :: String -> Bool -> IO ()
+xDoc prgmName cached = docServe cached True prgmName
 
-exec :: String -> IO ()
-exec fname = do
-  void (process fname)
+exec :: Command -> IO ()
+exec (RunFile fname) = xRun fname
+exec (BuildFile fname) = xBuild fname
+exec (Doc fname cached) = xDoc fname cached
+
+data Command
+  = BuildFile String
+  | RunFile String
+  | Doc String Bool
+
+cRun :: Parser Command
+cRun = RunFile <$> argument str (metavar "FILE" <> help "The file to run")
+
+cBuild :: Parser Command
+cBuild = BuildFile <$> argument str (metavar "FILE" <> help "The file to build")
+
+cDoc :: Parser Command
+cDoc = Doc
+  <$> argument str (metavar "FILE" <> help "The file to run")
+  <*> switch (long "cached" <> help "Cache results instead of reloading live (useful for serving rather than development)")
 
 main :: IO ()
 main = exec =<< execParser opts
   where
-    opts = info (runFile <**> helper)
+    opts = info (mainCommands <**> helper)
       ( fullDesc
      <> progDesc "Executes Catln options"
      <> header "Catln Compiler" )
+    mainCommands = subparser (
+         command "run" (info cRun (progDesc "Runs a program"))
+      <> command "build" (info cBuild (progDesc "Builds a program"))
+      <> command "doc" (info cDoc (progDesc "Runs webdocs for a program"))
+                             )
