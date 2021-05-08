ac4fdf401189f6cf9ee6675705f9b04ce2583d20
Zach Kimberg <zachary@kimberg.com>
2021-05-07 21:52:37 -0700

    Split repl and fix main
    This moves the repl code into a separate file in app for now. It also comments
    in out and I will work on fixing the repl in a later PR. The main is fixed to
    only support loading a single file for now, but does work

--- app/Main.hs
+++ app/Main.hs
@@ -2,58 +2,19 @@
 
 module Main where
 
-import qualified Data.HashMap.Strict as H
-
 import           Desugarf                 (desFiles)
--- import           Emit                     (codegen, initModule)
 import           Eval
-import           Syntax.Types
-import           Syntax.Prgm
-import           Syntax
 import           CRes
 import           Parser
 import           Parser.Syntax
 import           TypeCheck                (typecheckPrgm)
 
 import           Control.Monad
-import           Control.Monad.Trans
-import           Data.List                (isPrefixOf)
-
-import           System.Console.Haskeline
 import           System.Environment
-import Data.Graph
-import Utils
+-- import Repl (repl)
 
-type ReplEnv = ([RawStatement PreTyped], GraphData (RawPrgm PreTyped) String)
-
-coreImport :: String
-coreImport = "stack/core/main.ct"
-
-buildReplBaseEnv :: IO ReplEnv
-buildReplBaseEnv = do
-  rawCore <- readFiles True [coreImport]
-  case rawCore of
-    CRes _ r -> return ([], r)
-    CErr _ -> fail "Could not read core"
-
-parsingRepl :: ReplEnv -> String -> IO ReplEnv
-parsingRepl env source = case parseRepl source of
-    ReplErr err   -> print err >> return env
-    ReplExpr expr -> print expr >> return env
-    ReplStatement statement -> print statement >> return env
-
--- genRepl :: ReplEnv -> String -> IO ReplEnv
--- genRepl env source = do
---   let res = parseRepl source
---   case res of
---     ReplErr err -> print err >> return env
---     ReplExpr _ -> print ("Can not generate expression" :: String) >> return env
---     ReplStatement statement -> case typecheckPrgm $ desStatements [statement] of
---         TypeCheckResE err    -> print ("type check err: " ++ show err) >> return env
---         TypeCheckResult _ tprgm -> codegen initModule tprgm >> return env
-
-processDes :: CRes DesPrgm -> IO ()
-processDes des = case aux of
+processDes :: String -> CRes PPrgmGraphData -> IO ()
+processDes prgmName maybeRawPrgm = case aux of
   CErr err   -> print err
   CRes _ resIO -> do
     returnValue <- resIO
@@ -62,59 +23,20 @@ processDes des = case aux of
       (i, _) -> print $ "error code " ++ show i
   where
     aux = do
-      prgm <- des
-      tprgm <- typecheckPrgm prgm
-      evalMain "" (graphFromEdges [(tprgm, ("" :: String), [])])
+      rawPrgm <- maybeRawPrgm
+      desPrgm <- desFiles rawPrgm
+      tprgm <- typecheckPrgm desPrgm
+      evalMainx prgmName tprgm
 
-mainStatement :: RawExpr PreTyped -> RawStatement PreTyped
-mainStatement expr = RawDeclStatement $ RawDecl lhs [] (Just wrappedExpr)
-  where
-    lhs = DeclLHS emptyMetaN (Pattern (Object emptyMetaN FunctionObj "main" H.empty (H.singleton "io" (PreTyped (singletonType (PartialType (PTypeName "IO") H.empty H.empty H.empty PtArgAny)) Nothing, Nothing))) NoGuard)
-    wrappedExpr = RawMethods (RawValue emptyMetaN "io") [RawTupleApply emptyMetaN (emptyMetaN, RawValue emptyMetaN "println") [RawTupleArgNamed "msg" (RawMethods expr [RawValue emptyMetaN "toString"])]]
-
-processRepl :: ReplEnv -> String -> IO ReplEnv
-processRepl env@(envRepl, envCore) source = do
-  let res = parseRepl source
-  case res of
-    ReplErr err -> print err >> return env
-    ReplExpr expr -> do
-      -- main(IO io) = io.println(msg=expr.toString)
-      let replRawPrgm = ([coreImport], mainStatement expr:envRepl)
-      let des = desFiles (("replMain", replRawPrgm):envCore)
-      processDes des
-      return env
-    ReplStatement s -> return (s:envRepl, envCore)
-
-process :: [String] -> IO ()
+process :: String -> IO ()
 process source = do
-  raw <- readFiles True source
-  case raw of
-    CErr err -> print err
-    CRes _ raw' -> processDes (desFiles raw')
-
-processFile :: String -> IO ()
-processFile fname = process [fname, coreImport]
-
-repl :: IO ()
-repl = do
-  replBaseEnv <- buildReplBaseEnv
-  runInputT defaultSettings (loop replBaseEnv)
-  where loop env = do
-          minput <- getInputLine "eval> "
-          case minput of
-            Nothing -> outputStrLn "Goodbye."
-            Just input | ":q" `isPrefixOf` input -> outputStrLn "Goodbye."
-            Just input -> do
-              env' <- case input of
-                _ | ":p " `isPrefixOf` input -> lift $ parsingRepl env (drop 3 input)
-                -- _ | ":g " `isPrefixOf` input -> lift $ genRepl env (drop 3 input)
-                _ -> lift $ processRepl env input
-              loop env'
+  raw <- readFiles True [source]
+  processDes source raw
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    []      -> repl
-    [fname] -> void (processFile fname)
+    -- []      -> repl
+    [fname] -> void (process fname)
     _       -> putStr "Unknown arguments"
---
+++ src/Repl.hs
@@ -0,0 +1,90 @@
+
+module Repl where
+
+import qualified Data.HashMap.Strict as H
+
+import           Desugarf                 (desFiles)
+-- import           Emit                     (codegen, initModule)
+import           Eval
+import           Syntax.Types
+import           Syntax.Prgm
+import           Syntax
+import           CRes
+import           Parser
+import           Parser.Syntax
+import           TypeCheck                (typecheckPrgm)
+
+import           Control.Monad
+import           Control.Monad.Trans
+import           Data.List                (isPrefixOf)
+
+import           System.Console.Haskeline
+import           System.Environment
+import Data.Graph
+import Utils
+
+type ReplEnv = ([RawStatement PreTyped], GraphData (RawPrgm PreTyped) String)
+
+coreImport :: String
+coreImport = "stack/core/main.ct"
+
+buildReplBaseEnv :: IO ReplEnv
+buildReplBaseEnv = do
+  rawCore <- readFiles True [coreImport]
+  case rawCore of
+    CRes _ r -> return ([], r)
+    CErr _ -> fail "Could not read core"
+
+parsingRepl :: ReplEnv -> String -> IO ReplEnv
+parsingRepl env source = case parseRepl source of
+    ReplErr err   -> print err >> return env
+    ReplExpr expr -> print expr >> return env
+    ReplStatement statement -> print statement >> return env
+
+-- genRepl :: ReplEnv -> String -> IO ReplEnv
+-- genRepl env source = do
+--   let res = parseRepl source
+--   case res of
+--     ReplErr err -> print err >> return env
+--     ReplExpr _ -> print ("Can not generate expression" :: String) >> return env
+--     ReplStatement statement -> case typecheckPrgm $ desStatements [statement] of
+--         TypeCheckResE err    -> print ("type check err: " ++ show err) >> return env
+--         TypeCheckResult _ tprgm -> codegen initModule tprgm >> return env
+
+mainStatement :: RawExpr PreTyped -> RawStatement PreTyped
+mainStatement expr = RawDeclStatement $ RawDecl lhs [] (Just wrappedExpr)
+  where
+    lhs = DeclLHS emptyMetaN (Pattern (Object emptyMetaN FunctionObj "main" H.empty (H.singleton "io" (PreTyped (singletonType (PartialType (PTypeName "IO") H.empty H.empty H.empty PtArgAny)) Nothing, Nothing))) NoGuard)
+    wrappedExpr = RawMethods (RawValue emptyMetaN "io") [RawTupleApply emptyMetaN (emptyMetaN, RawValue emptyMetaN "println") [RawTupleArgNamed "msg" (RawMethods expr [RawValue emptyMetaN "toString"])]]
+
+
+processRepl :: ReplEnv -> String -> IO ReplEnv
+processRepl = undefined
+-- processRepl env@(envRepl, envCore) source = do
+--   let res = parseRepl source
+--   case res of
+--     ReplErr err -> print err >> return env
+--     ReplExpr expr -> do
+--       -- main(IO io) = io.println(msg=expr.toString)
+--       let replRawPrgm = ([coreImport], mainStatement expr:envRepl)
+--       let des = desFiles (("replMain", replRawPrgm):envCore)
+--       processDes des
+--       return env
+--     ReplStatement s -> return (s:envRepl, envCore)
+
+
+repl :: IO ()
+repl = do
+  replBaseEnv <- buildReplBaseEnv
+  runInputT defaultSettings (loop replBaseEnv)
+  where loop env = do
+          minput <- getInputLine "eval> "
+          case minput of
+            Nothing -> outputStrLn "Goodbye."
+            Just input | ":q" `isPrefixOf` input -> outputStrLn "Goodbye."
+            Just input -> do
+              env' <- case input of
+                _ | ":p " `isPrefixOf` input -> lift $ parsingRepl env (drop 3 input)
+                -- _ | ":g " `isPrefixOf` input -> lift $ genRepl env (drop 3 input)
+                _ -> lift $ processRepl env input
+              loop env'
