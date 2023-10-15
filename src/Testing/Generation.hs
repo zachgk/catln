4b2e52a23d502106760209a90ad926c4128208ea
Zach Kimberg <zachary@kimberg.com>
2023-10-15 10:39:46 -0700

    Split test generation into new module
    This moves the test generation into a new module and into source where it can be
    used in additional kinds of testing.

---
+++ src/Testing/Generation.hs
@@ -0,0 +1,103 @@
+--------------------------------------------------------------------
+-- |
+-- Module    :  Testing.Generation
+-- Copyright :  (c) Zach Kimberg 2023
+-- License   :  MIT
+-- Maintainer:  zachary@kimberg.com
+-- Stability :  experimental
+-- Portability: non-portable
+--
+-- This module defines arbitrary code generation for property testing.
+-- It helps test both the compiler itself and code written in it.
+--------------------------------------------------------------------
+
+module Testing.Generation where
+
+import qualified Data.HashMap.Strict as H
+import           Data.Maybe
+import           Hedgehog
+import qualified Hedgehog.Gen        as HG
+import           Semantics           (getExprType, oaObjPath)
+import           Semantics.Prgm
+import           Semantics.Types
+import           Text.Printf
+import           Utils
+
+genTypeFromExpr :: ExprPrgm Expr () -> Expr () -> Gen PartialType
+genTypeFromExpr _ (CExpr _ c) = return $ constantPartialType c
+genTypeFromExpr _ (Value _ n) = return $ partialVal $ PTypeName n
+genTypeFromExpr prgm (TupleApply _ (_, baseExpr) oa) = do
+  base@PartialType{ptArgs=baseArgs} <- genTypeFromExpr prgm baseExpr
+  shouldAddArg <- HG.bool
+  if shouldAddArg
+    then case oaArr oa of
+           Just (GuardExpr arrExpr _) -> do
+             arrExpr' <- genTypeFromExpr prgm arrExpr
+             return base{ptArgs = H.insert (oaObjPath oa) (singletonType arrExpr') baseArgs}
+           Nothing -> return base{ptArgs = H.insert (oaObjPath oa) (getMetaType $ oaM oa) baseArgs}
+    else return base
+genTypeFromExpr prgm (VarApply _ baseExpr varName m) = do
+  base@PartialType{ptVars=baseVars} <- genTypeFromExpr prgm baseExpr
+  shouldAddVar <- HG.bool
+  return $ if shouldAddVar
+    then base{ptVars = H.insert varName (getMetaType m) baseVars}
+    else base
+genTypeFromExpr _ e = error $ printf "Unimplemented genTypeFromExpr for %s" (show e)
+
+genType :: ExprPrgm Expr () -> Gen Type
+genType prgm@(objMap, ClassGraph cg, _) = HG.choice gens
+  where
+
+    gens = if graphEmpty cg
+      then [genBasic]
+      else [genBasic, genCGOld, genCG, genCGRel, genObjM, genObj]
+
+    genBasic :: Gen Type
+    genBasic = HG.element [topType, bottomType]
+
+    genCGOld :: Gen Type
+    genCGOld = do
+      classNode <- HG.element $ graphToNodes cg
+      return $ typeVal $ snd3 classNode
+
+    genCG :: Gen Type
+    genCG = do
+      classNode <- HG.element $ graphToNodes cg
+      return $ case snd3 classNode of
+        cls@PClassName{} -> TopType [PredClass $ partialVal cls]
+        t                -> typeVal t
+
+    genCGRel :: Gen Type
+    genCGRel = do
+      classNode <- HG.element $ graphToNodes cg
+      return $ TopType [PredRel $ partialVal $ PRelativeName $ fromPartialName $ snd3 classNode]
+
+    genObjM :: Gen Type
+    genObjM = do
+      oa <- HG.element objMap
+      let (GuardExpr objExpr Nothing) = fromJust $ oaObj oa
+      return $ getExprType objExpr
+
+    genObj :: Gen Type
+    genObj = do
+      oa <- HG.element objMap
+      let (GuardExpr objExpr Nothing) = fromJust $ oaObj oa
+      singletonType <$> genTypeFromExpr prgm objExpr
+
+genPartialType :: ExprPrgm Expr () -> Gen PartialType
+genPartialType prgm@(objMap, ClassGraph cg, _) = do
+  gen <- if graphEmpty cg
+    then HG.discard
+    else return [genCG, genObj]
+  HG.choice gen
+  where
+    genCG :: Gen PartialType
+    genCG = do
+      classNode <- HG.element $ graphToNodes cg
+      return $ partialVal $ snd3 classNode
+
+    genObj :: Gen PartialType
+    genObj = do
+      oa <- HG.element objMap
+      let (GuardExpr objExpr Nothing) = fromJust $ oaObj oa
+      genTypeFromExpr prgm objExpr
--- test/Semantics/TypesTests.hs
+++ test/Semantics/TypesTests.hs
@@ -7,13 +7,13 @@ import qualified Data.HashMap.Strict as H
 import           Data.Maybe
 import           Hedgehog
 import qualified Hedgehog.Gen        as HG
-import           Semantics           (getExprType, oaObjPath)
 import           Semantics.Prgm
 import           Semantics.Types
 import           Syntax.Ct.Desugarf  (desFiles)
 import           Syntax.Parsers      (readFiles)
 import           Test.Tasty
 import           Test.Tasty.Hedgehog as HG
+import           Testing.Generation
 import           Text.Printf
 import           TypeCheck           (typecheckPrgm)
 import           Utils
@@ -30,85 +30,6 @@ findPrgms = do
     let tprgm = fromCRes $ typecheckPrgm prgm
     return (fileName, mergeExprPrgms $ map fst3 $ graphToNodes tprgm)
   return (H.insert "empty" emptyExprPrgm $ H.fromList prgms)
-
-genTypeFromExpr :: ExprPrgm Expr () -> Expr () -> Gen PartialType
-genTypeFromExpr _ (CExpr _ c) = return $ constantPartialType c
-genTypeFromExpr _ (Value _ n) = return $ partialVal $ PTypeName n
-genTypeFromExpr prgm (TupleApply _ (_, baseExpr) oa) = do
-  base@PartialType{ptArgs=baseArgs} <- genTypeFromExpr prgm baseExpr
-  shouldAddArg <- HG.bool
-  if shouldAddArg
-    then case oaArr oa of
-           Just (GuardExpr arrExpr _) -> do
-             arrExpr' <- genTypeFromExpr prgm arrExpr
-             return base{ptArgs = H.insert (oaObjPath oa) (singletonType arrExpr') baseArgs}
-           Nothing -> return base{ptArgs = H.insert (oaObjPath oa) (getMetaType $ oaM oa) baseArgs}
-    else return base
-genTypeFromExpr prgm (VarApply _ baseExpr varName m) = do
-  base@PartialType{ptVars=baseVars} <- genTypeFromExpr prgm baseExpr
-  shouldAddVar <- HG.bool
-  return $ if shouldAddVar
-    then base{ptVars = H.insert varName (getMetaType m) baseVars}
-    else base
-genTypeFromExpr _ e = error $ printf "Unimplemented genTypeFromExpr for %s" (show e)
-
-genType :: ExprPrgm Expr () -> Gen Type
-genType prgm@(objMap, ClassGraph cg, _) = HG.choice gens
-  where
-
-    gens = if graphEmpty cg
-      then [genBasic]
-      else [genBasic, genCGOld, genCG, genCGRel, genObjM, genObj]
-
-    genBasic :: Gen Type
-    genBasic = HG.element [topType, bottomType]
-
-    genCGOld :: Gen Type
-    genCGOld = do
-      classNode <- HG.element $ graphToNodes cg
-      return $ typeVal $ snd3 classNode
-
-    genCG :: Gen Type
-    genCG = do
-      classNode <- HG.element $ graphToNodes cg
-      return $ case snd3 classNode of
-        cls@PClassName{} -> TopType [PredClass $ partialVal cls]
-        t                -> typeVal t
-
-    genCGRel :: Gen Type
-    genCGRel = do
-      classNode <- HG.element $ graphToNodes cg
-      return $ TopType [PredRel $ partialVal $ PRelativeName $ fromPartialName $ snd3 classNode]
-
-    genObjM :: Gen Type
-    genObjM = do
-      oa <- HG.element objMap
-      let (GuardExpr objExpr Nothing) = fromJust $ oaObj oa
-      return $ getExprType objExpr
-
-    genObj :: Gen Type
-    genObj = do
-      oa <- HG.element objMap
-      let (GuardExpr objExpr Nothing) = fromJust $ oaObj oa
-      singletonType <$> genTypeFromExpr prgm objExpr
-
-genPartialType :: ExprPrgm Expr () -> Gen PartialType
-genPartialType prgm@(objMap, ClassGraph cg, _) = do
-  gen <- if graphEmpty cg
-    then HG.discard
-    else return [genCG, genObj]
-  HG.choice gen
-  where
-    genCG :: Gen PartialType
-    genCG = do
-      classNode <- HG.element $ graphToNodes cg
-      return $ partialVal $ snd3 classNode
-
-    genObj :: Gen PartialType
-    genObj = do
-      oa <- HG.element objMap
-      let (GuardExpr objExpr Nothing) = fromJust $ oaObj oa
-      genTypeFromExpr prgm objExpr
 
 propCompactNoChanges :: Prgms -> Property
 propCompactNoChanges prgms = property $ do
