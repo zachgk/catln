8b03bec92298809121678375ab7857ce4be17214
Zach Kimberg <zachary@kimberg.com>
2023-10-29 04:54:37 -0700

    Move mt and mtd test helper to integ
    This keeps the integ helpers as part of the integ module rather than in the main Spec.

--- test/Integration/Integ.hs
+++ test/Integration/Integ.hs
@@ -110,3 +110,19 @@ standardTests = findCt testDir
 
 integrationTests :: IO TestTree
 integrationTests = runTests True <$> standardTests
+
+mt :: String -> IO ()
+mt k = do
+  let fileName = testDir ++ k ++ ".ct"
+  tests <- standardTests
+  if fileName `elem` tests
+     then defaultMain $ runTests True [fileName]
+     else error $ printf "invalid test name %s in %s" fileName (show tests)
+
+mtd :: String -> IO ()
+mtd k = do
+  let fileName = testDir ++ k ++ ".ct"
+  tests <- standardTests
+  if fileName `elem` tests
+     then docApi False True fileName
+     else error $ printf "invalid test name %s in %s" fileName (show tests)
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,26 +3,7 @@ module Main where
 import           Integration.Integ
 import           Semantics.TypesTests     (typeTests)
 import           Test.Tasty
-import           Text.Printf
 import           Typecheck.TypeCheckTests (typecheckTests)
-import           WebDocs                  (docApi)
-
-
-mt :: String -> IO ()
-mt k = do
-  let fileName = testDir ++ k ++ ".ct"
-  tests <- standardTests
-  if fileName `elem` tests
-     then defaultMain $ runTests True [fileName]
-     else error $ printf "invalid test name %s in %s" fileName (show tests)
-
-mtd :: String -> IO ()
-mtd k = do
-  let fileName = testDir ++ k ++ ".ct"
-  tests <- standardTests
-  if fileName `elem` tests
-     then docApi False True fileName
-     else error $ printf "invalid test name %s in %s" fileName (show tests)
 
 main :: IO ()
 main = do
