491cded26a26fa9d886a31580aad6b4aa9fb2d7f
Zach Kimberg <zachary@kimberg.com>
2024-06-29 01:41:57 -0700

    Replace project dir in golden tests
    This replaces the directory to this repo saved in the golden tests with a
    generic path. Before, this meant the tests would fail outside of my development
    machine such as on github actions. This should fix that problem.

--- test/Integration/Integ.hs
+++ test/Integration/Integ.hs
@@ -13,7 +13,7 @@ import           Eval
 import           Syntax.Ct.Desugarf (desFiles)
 import           Syntax.Parsers     (mkDesCanonicalImportStr,
                                      mkRawCanonicalImportStr, readFiles)
-import           System.Directory   (doesFileExist)
+import           System.Directory   (doesFileExist, getCurrentDirectory)
 import           System.FilePath    (takeBaseName)
 import           Text.Pretty.Simple (pShowNoColor)
 import           TypeCheck
@@ -28,6 +28,22 @@ goldenDesugarDir = "test/Integration/goldenDesugar/"
 
 goldenTypecheckDir :: String
 goldenTypecheckDir = "test/Integration/goldenTypecheck/"
+
+runGoldenTest :: (Show fn, Show prgm) => String -> String -> String -> GraphData fn prgm -> (String -> IO ()) -> IO ()
+runGoldenTest goldenType goldenDir fileNameStr prgm step = do
+  let goldenPath = goldenDir ++ takeBaseName fileNameStr ++ ".txt"
+  goldenExists <- doesFileExist goldenPath
+  let showPrgm = pShowNoColor $ graphToNodes prgm
+  cwd <- getCurrentDirectory
+  let showPrgm' = T.unpack $ T.replace (T.pack cwd) "/repo/dir" showPrgm
+  if testDir `isPrefixOf` fileNameStr && goldenExists
+    then do
+      step $ printf "Golden test %s..." goldenType
+      golden <- readFile goldenPath
+      when (golden /= showPrgm') (assertFailure $ printf "%s doesn't match golden test" goldenType)
+    else do
+      step $ printf "No golden test for %s. Writing" goldenType
+      writeFile goldenPath showPrgm'
 
 runTest :: Bool -> Bool -> String -> TestTree
 runTest runGolden includeCore fileNameStr = testCaseSteps fileNameStr $ \step -> do
@@ -45,17 +61,7 @@ runTest runGolden includeCore fileNameStr = testCaseSteps fileNameStr $ \step ->
         CRes _ prgm -> do
 
           when runGolden $ do
-            let goldenDesugarPath = goldenDesugarDir ++ takeBaseName fileNameStr ++ ".txt"
-            goldenDesugarExists <- doesFileExist goldenDesugarPath
-            let showPrgm = T.unpack $ pShowNoColor $ graphToNodes prgm
-            if testDir `isPrefixOf` fileNameStr && goldenDesugarExists
-              then do
-                step "Golden test desugar..."
-                golden <- readFile goldenDesugarPath
-                when (golden /= showPrgm) (assertFailure "Desugar doesn't match golden test" )
-              else do
-                step "No golden test for desugar. Writing"
-                writeFile goldenDesugarPath showPrgm
+            runGoldenTest "desugar" goldenDesugarDir fileNameStr prgm step
 
           step "Typecheck..."
           case typecheckPrgm prgm of
@@ -64,17 +70,7 @@ runTest runGolden includeCore fileNameStr = testCaseSteps fileNameStr $ \step ->
             CRes _ tprgm -> do
 
               when runGolden $ do
-                let goldenTypecheckPath = goldenTypecheckDir ++ takeBaseName fileNameStr ++ ".txt"
-                goldenTypecheckExists <- doesFileExist goldenTypecheckPath
-                let showTPrgm = T.unpack $ pShowNoColor $ graphToNodes tprgm
-                if testDir `isPrefixOf` fileNameStr && goldenTypecheckExists
-                  then do
-                    step "Golden test typecheck..."
-                    golden <- readFile goldenTypecheckPath
-                    when (golden /= showTPrgm) (assertFailure "Typecheck doesn't match golden test" )
-                  else do
-                    step "No golden test for typecheck. Writing"
-                    writeFile goldenTypecheckPath showTPrgm
+                runGoldenTest "typecheck" goldenTypecheckDir fileNameStr tprgm step
 
               when (evalRunnable $ evalTargetMode "main" fileName tprgm) $ do
                 step "Eval Run..."
