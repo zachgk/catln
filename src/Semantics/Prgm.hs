8832e2b19aaf13ea0c35dec4982678628cb408c0
Zach Kimberg <zachary@kimberg.com>
2024-08-06 08:32:21 -0700

    Generalize operatorHasArrow matching
    Before, it worked only with the rhs being a TypeVar. This change makes it work
    for any RHS, because the core if it comes from intersection.

--- src/Semantics/Prgm.hs
+++ src/Semantics/Prgm.hs
@@ -36,6 +36,7 @@ import           Semantics.TypeGraph (ReachesTree)
 import           Semantics.Types
 import           Text.Megaparsec
 import           Text.Printf
+import           Utils               (unionsWith)
 
 newtype Import = Import String
   deriving (Eq, Ord, Show)
@@ -265,10 +266,11 @@ instance ExprClass Expr where
       aux (EWhere _ base cond) = case maybeExprPath cond of
         Just n | n == operatorHasArrow -> case (H.lookup (partialKey operatorArgL) condArgs, H.lookup (partialKey operatorArgR) condArgs) of
                   (Just (Just (_, Just l)), Just (Just (_, Just r))) -> case (getExprType $ exprPropagateTypes l, getExprType r) of
-                    (lt@UnionType{}, TypeVar rt _) -> case typeGraphQuery typeEnv (fmap snd base') $ fromJust $ maybeGetSingleton lt of
+                    (lt@UnionType{}, rt) -> case typeGraphQuery typeEnv (fmap snd base') $ fromJust $ maybeGetSingleton lt of
                       [] -> H.empty
-                      [qr] -> H.singleton rt ([], qr)
-                      qr -> error $ printf "Not yet implemented searching arrow %s to %s. Found %s" (show lt) (show rt) (show qr)
+                      qr -> let fromEachArrow = fmap (\qrt -> fst $ intersectTypesWithVarEnv typeEnv (fmap snd base') qrt rt) qr
+                                fromAllArrows = ([],) <$> unionsWith (unionTypes typeEnv) fromEachArrow
+                             in H.unionWith merge base' fromAllArrows
                     (lt, rt) -> error $ printf "Found invalid types of l=%s and r=%s in ?->" (show lt) (show rt)
                   _ -> error $ printf "Could not find /l and /r in %s in ?->" (show condArgs)
         _ -> base'
